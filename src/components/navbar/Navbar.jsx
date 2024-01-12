c7e1d49edd2f67f6850b7e2754112cf3e7e14737
adnan <azizart320480@gamil.com>
2024-01-12 21:14:40 +0500

    some change

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,32 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React, { useState } from 'react'
 import logo from '../../assets/logo.png'
+import { AiOutlineUser } from "react-icons/ai"
+import { IoCartOutline } from "react-icons/io5";
+
 export const Navbar = () => {
 
   return (
     <>
       <Box style={{
-        height:'85px',
-        width:'100%',
+        height: '85px',
+        width: '100%',
         // border:'1px solid red',
-        backgroundColor:'aqua'
+        backgroundColor: 'aqua',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent:'space-between',
+        padding:'0px 6rem'
       }}>
-        <Box>
-          <img style={{width:'100px'}} src={logo} alt="" />
+        <Box style={{ display: 'flex', alignItems: 'center', gap: '12px' }}>
+          <img style={{ width: '70px' }} src={logo} alt="" />
+          <Typography>Log in</Typography>
+          <Typography>Admin panel</Typography>
+        </Box>
+        <Box style={{ display: 'flex', alignItems: 'center', gap: '9px' ,opacity:'.800', fontSize:'1.4rem'}}>
+          <Typography>Log out</Typography>
+          <AiOutlineUser />
+          <IoCartOutline />
         </Box>
       </Box>
     </>
