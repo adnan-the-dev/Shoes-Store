8ed65f555fcdb662bace5ef04c8818380bcfc392
adnan <azizart320480@gmail.com>
2024-01-15 19:00:17 +0500

    some

--- src/components/homePage/Home.jsx
+++ src/components/homePage/Home.jsx
@@ -4,8 +4,8 @@ import Adidas from '../../assets/Adidas.webp'
 import Nike from '../../assets/Nike.webp'
 import Bata from '../../assets/Bata.webp'
 import Service from '../../assets/Service.webp'
-import { Box, Typography } from '@mui/material'
-import { CatagoryBox, CategoryTag, ChildCatagoryBox, ImageBox, TypographyBox } from './styled-component'
+import { Box, Grid } from '@mui/material'
+import { CatagoryBox, CategoryTag, ChildCatagoryBox, ImageBox, MainSilderBox, SilderTag, TypographyBox } from './styled-component'
 
 
 export default function Home() {
@@ -41,8 +41,44 @@ export default function Home() {
             </CategoryTag>
         </ChildCatagoryBox>
     </CatagoryBox>
+
+     {/* *********Silder Section************ */}
+
+    <MainSilderBox>
+        <SilderTag>By Adidas</SilderTag>
+        <Grid container>
+            <Grid>
+
+            </Grid>
+        </Grid>
+    </MainSilderBox>
     </>
   )
 }
 
 
+{/* <Grid container>
+        <Grid item xs={12} sm={12} md={6} lg={8} xl={8}>
+          <Basic>
+            <TagHeading>Feat wear Paradies</TagHeading>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Blanditiis commodi cum cupiditate ducimus, fugit harum id
+              necessitatibus odio quam quasi, quibusdam rem tempora voluptates.
+              Cumque debitis dignissimos id quam vel!
+            </Typography>
+            <ButtonBox>
+              <Button variant="contained" color="success" onClick={() => {loginRoute()}}> Log in</Button>
+              <Button variant="outlined" color="primary" onClick={() => {adminPanel()}}> Admin panel</Button>
+            </ButtonBox>
+          </Basic>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
+          <ImageBox>
+            <img
+              src="https://shoe-paradies.vercel.app/_next/image?url=%2Fwal.avif&w=1920&q=75"
+              alt=""
+            />
+          </ImageBox>
+        </Grid>
+      </Grid> */}
--- src/components/homePage/styled-component.jsx
+++ src/components/homePage/styled-component.jsx
@@ -7,30 +7,41 @@ export const CatagoryBox = styled(Box)({
 });
 
 export const ChildCatagoryBox = styled(Box)({
-    backgroundColor: '#edeef0'
+  backgroundColor: "#edeef0",
 });
 
 export const ImageBox = styled(Box)({
-    height:'350px',
-    width:'370px',
-    padding:'2px 2px'
+  height: "350px",
+  width: "370px",
+  padding: "2px 2px",
 });
 
 export const CategoryTag = styled(Box)({
-    display:'flex',
-    justifyContent:'center',
-    // alignContent:'center',
-    // textAlign:'center',
-    
+  display: "flex",
+  justifyContent: "center",
 });
 
 export const TypographyBox = styled(Typography)({
-    fontWeight: '600',
-    fontSize: '1.25rem',
-    lineHeight: '1.75rem',
-    cursor:'pointer',
-    "&:hover": {
-        backgroundColor: '#000',
-        color:'#fff',
-      }
+  fontWeight: "600",
+  fontSize: "1.25rem",
+  lineHeight: "1.75rem",
+  cursor: "pointer",
+  "&:hover": {
+    backgroundColor: "#000",
+    color: "#fff",
+  },
+});
+// *********Silder Section************
+
+export const MainSilderBox = styled(Box)({
+  marginTop: '3rem!important',
+});
+
+export const SilderTag = styled(Box)({
+  marginLeft: "2.5rem",
+  fontWeight: "bold",
+  fontWeight: "600",
+  fontSize: "1.25rem",
+  lineHeight: "1.75rem",
+  marginBottom: '3rem!important',
 });
