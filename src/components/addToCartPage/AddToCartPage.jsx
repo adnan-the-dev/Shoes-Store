8ab3618d134b9f60afff6b2109e9afa751a06804
adnan <azizart320480@gmail.com>
2024-01-17 18:58:58 +0500

    some change

--- src/components/addToCartPage/AddToCartPage.jsx
+++ src/components/addToCartPage/AddToCartPage.jsx
@@ -1,12 +1,27 @@
-import { Grid, Typography } from '@mui/material'
 import React from 'react'
-import {MainContainerBox } from './styled-component'
-
+import { ChildContainer, ChildImage, ImageBox, ImageTextBox, LargeImage, LargeImg, MainContainerBox, SmallImge } from './styled-component'
+import Adidas from '../../assets/Adidas.webp'
+import racer from '../../assets/racer.webp'
 export function AddToCartPage() {
     return (
         <>
             <MainContainerBox>
+                <ChildContainer>
+                    <ImageBox>
+                        <ChildImage>
+                            <SmallImge component='img' src={Adidas} alt="" />
+                            <SmallImge isActive={true} component='img' src={Adidas} alt="" />
+                            <SmallImge isActive={true} component='img' src={Adidas} alt="" />
+                        </ChildImage>
+                        <LargeImage>
+                            <LargeImg component='img' src={racer} alt="" />
+                        </LargeImage>
+                    </ImageBox>
 
+                    <ImageTextBox>
+                        hello
+                    </ImageTextBox>
+                </ChildContainer>
             </MainContainerBox>
         </>
     )
--- src/components/addToCartPage/styled-component.jsx
+++ src/components/addToCartPage/styled-component.jsx
@@ -4,5 +4,55 @@ import { Box, Typography, styled } from "@mui/material";
 
 export const MainContainerBox = styled(Box)({
     paddingTop: '4rem',
-    padding: '4rem 5rem',
+    width: '100%'
+    // padding: '4rem 5rem',
+});
+
+export const ChildContainer = styled(Box)({
+    display: 'flex',
+    width: '75%',
+    margin: 'auto!important',
+});
+
+export const ImageBox = styled(Box)({
+    width: '50%',
+    marginBottom: '0',
+    display: 'flex'
+});
+
+export const ChildImage = styled(Box)({
+
+});
+export const SmallImge = styled(Box)(({ isActive }) => ({
+    color: 'transparent',
+    width: '100px',
+    height: '100px',
+    maxWidth: '100%',
+    height: 'auto',
+    display: 'block',
+    verticalAlign: 'middle',
+    marginTop: isActive ? '.5rem' : '0rem'
+}));
+
+export const LargeImage = styled(Box)({
+    marginLeft: '0.5rem',
+    width: '100%'
+});
+
+export const LargeImg = styled(Box)({
+    color: 'transparent',
+    margin: 'auto',
+    maxWidth: '100%',
+    height: 'auto',
+    display: 'block',
+    verticalAlign: 'middle',
+    aspectRatio: '1/1',
+    width: '100%',
+    objectFit: 'cover'
+});
+
+
+
+export const ImageTextBox = styled(Box)({
+
 });
